5eaa20898f05d7244bb370dc8a110f55ba67cf6d
anguscheng2508 <72135270+anguscheng2508@users.noreply.github.com>
2025-04-02 21:04:41 +0100

    Final submission
    Connection and control via backend fully working.
    All sensors / actuators working as intended

--- iot-test/iot-test.ino
+++ iot-test/iot-test.ino
@@ -8,7 +8,7 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <time.h>
-
+#include <WebSocketsClient.h>
 
 // WiFi Credentials
 //const char* ssid = "eir53845773";       // Replace with your WiFi SSID
@@ -20,8 +20,10 @@
 const char* ssid = "Gwishy";       // Replace with your WiFi SSID
 const char* password = "angus321"; // Replace with your WiFi password
 
-// AWS EC2 API URL (Use your actual API endpoint)
-const char* serverUrl = "https://8601-89-101-47-26.ngrok-free.app";
+// AWS EC2 API URL 
+const char* serverUrl = "f858-134-226-214-245.ngrok-free.app";
+
+WebSocketsClient webSocket;
 
 // Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
 const int trigPin = 13;
@@ -36,9 +38,17 @@ Servo windowServo;
 
 const int tempPin = 1;
 const int fanPin = 4;
+
 AccelStepper myStepper(AccelStepper::FULL4WIRE, 5, 6, 7, 8);
 long targetPos = 0;
 bool pumpIsOn = false;
+
+bool overrideLamp = false;
+bool overrideFan = false;
+bool overridePump = false;
+bool overrideAlarm = false;
+bool overrideWindow = false;
+
 
 unsigned long previousmillisUS = 0;
 const long intervalUS = 500; //(micro seconds => 1/1000 => 1000 microseconds = 1 second)
@@ -50,6 +60,7 @@ unsigned long previousmillisAIR = 0;
 const long intervalAIR = 1000;
 
 String inputString = "";  // stores the user input
+
 
 int usSensor(int trigPin, int echoPin) {
   long distance;
@@ -64,6 +75,124 @@ int usSensor(int trigPin, int echoPin) {
   duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2;
   return distance;
+}
+
+
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
+  if (type == WStype_TEXT) {
+    Serial.print("Received: ");
+    Serial.println((char*)payload);
+
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, payload);
+    if (error) {
+      Serial.print("JSON parse failed: ");
+      Serial.println(error.c_str());
+      return;
+    }
+
+    // Handle "message":"Connected"
+    if (doc.containsKey("message")) {
+      String msg = doc["message"];
+      if (msg == "Connected") {
+        Serial.println("Server acknowledged connection.");
+      }
+    }
+
+    // Handle "device":"device" style messages too
+    if (doc.containsKey("device") && doc.containsKey("action")) {
+      String device = doc["device"];
+      String action = doc["action"];
+
+      
+      if (device == "LAMP") {
+        if (action == "ON") {
+          digitalWrite(lampPin, HIGH);
+          overrideLamp = true;
+          Serial.println("Lamp turned ON");
+          
+        } else if (action == "OFF") {
+          digitalWrite(lampPin, LOW);
+          overrideLamp = true;
+          Serial.println("Lamp turned OFF");
+          
+        } else if (action == "AUTO") {
+          overrideLamp = false;
+          Serial.println("Lamp control back to automatic.");
+        }
+      }
+
+      if (device == "ALARM") {
+        if (action == "ON") {
+          digitalWrite(alarmPin, HIGH);
+          overrideAlarm = true;
+          Serial.println("Alarm turned ON");
+          
+        } else if (action == "OFF") {
+          digitalWrite(alarmPin, LOW);
+          overrideAlarm = true;
+          Serial.println("Alarm turned OFF");
+          
+        } else if (action == "AUTO") {
+          overrideAlarm = false;
+          Serial.println("Alarm control back to automatic.");
+        }
+      }
+
+      if (device == "FAN") {
+        if (action == "ON") {
+          digitalWrite(fanPin, LOW);
+          overrideFan = true;
+          Serial.println("Fan turned ON");
+          
+        } else if (action == "OFF") {
+          digitalWrite(fanPin, HIGH);
+          overrideFan = true;
+          Serial.println("fan turned OFF");
+          
+        } else if (action == "AUTO") {
+          overrideFan = false;
+          Serial.println("Fan control back to automatic.");
+        }
+      }
+
+      if (device == "PUMP") {
+        if (action == "ON") {
+          overridePump = true;
+          targetPos += 2048; 
+          myStepper.moveTo(targetPos);
+          Serial.println("Pump turned ON");
+          
+        } else if (action == "OFF") {
+            overridePump = true;
+            targetPos -= 2048;          
+            myStepper.moveTo(targetPos);
+            Serial.println("Pump turned OFF");
+            
+        } else if (action == "AUTO") {
+          overridePump = false;
+          Serial.println("Pump control back to automatic.");
+        }
+      }
+
+      if (device == "WINDOW") {
+        if (action == "ON") {
+          windowServo.write(180);
+          overrideWindow = true;
+          Serial.println("Window opened");
+          
+        } else if (action == "OFF") {
+          windowServo.write(0);
+          overrideWindow = true;
+          Serial.println("Window closed");
+          
+        } else if (action == "AUTO") {
+          overrideWindow = false;
+          Serial.println("Window control back to automatic.");
+        }
+      }
+    }
+  }
 }
 
 
@@ -90,11 +219,6 @@ void connectWiFi() {
   }
 
   Serial.println(&timeinfo, "Time: %Y-%m-%d %H:%M:%S");
-  
-    // Set up WebSocket
-//  webSocket.begin("your.server.ip.or.url", 80, "/ws"); // Replace with your server IP/domain and port/path
-//  webSocket.onEvent(webSocketEvent);
-//  webSocket.setReconnectInterval(5000);
 }
 
 
@@ -103,8 +227,7 @@ void sendSensorData(String sensorName, String timestamp, int sensorValue) {
     if (WiFi.status() == WL_CONNECTED) {
         HTTPClient http;
         
-        // You might need to append a specific path to your base URL
-        String endpoint = String(serverUrl) + "/sensor-data";  // Modify this path as needed
+        String endpoint = "https://" + String(serverUrl) + "/sensor-data";  // Modify this path as needed
         Serial.print("Sending data to: ");
         Serial.println(endpoint);
         
@@ -113,9 +236,7 @@ void sendSensorData(String sensorName, String timestamp, int sensorValue) {
 
         // Create JSON Data
         StaticJsonDocument<200> jsonDoc;
-        jsonDoc["sensor"] = sensorName;  // Changed from object to string with valid enum value
-        
-        // Get current timestamp (if you have a way to get real time)
+        jsonDoc["sensor"] = sensorName;
         jsonDoc["timestamp"] = timestamp;  
         jsonDoc["value"] = sensorValue;
 
@@ -147,7 +268,6 @@ void sendSensorData(String sensorName, String timestamp, int sensorValue) {
 }
 
 
-
 String getTimestamp() {
     struct tm timeinfo;
     time_t now;
@@ -160,9 +280,15 @@ String getTimestamp() {
 }
 
 
+
 void setup() {
   Serial.begin(115200);
   connectWiFi();
+//  String websocketString = "wss://" + String(serverUrl); //"wss://" + "f858-134-226-214-245.ngrok-free.app";
+//  webSocket.beginSSL(websocketString, 443, "/ws/connect/ok");
+  webSocket.beginSSL("4ac9-134-226-214-244.ngrok-free.app", 443, "/ws/connect/ok");
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(5000);
   
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -176,49 +302,22 @@ void setup() {
   
   pinMode(alarmPin, OUTPUT);
 
-  myStepper.setMaxSpeed(1500);     // steps/sec
+  myStepper.setMaxSpeed(200);     // steps/sec
   myStepper.setAcceleration(100);  // steps/sec^2
   myStepper.setCurrentPosition(0);
 }
 
 
 void loop() {
-//  webSocket.loop();
-  Serial.println("------------------------------------------------------------");
+//  Serial.println("------------------------------------------------------------");
+  webSocket.loop();
+  myStepper.run();  // must be called often!
+
   String timestamp = getTimestamp();
   if (Serial.available()) {
     inputString = Serial.readStringUntil('\n');
     inputString.trim();
   }
-
-//  unsigned long currentMillis = millis();
-//  if (currentMillis - previousMillis >= interval) {
-//    previousMillis = currentMillis;
-//    // Simulate receiving a message (you can change this string each time)
-//    const char* incomingJson = "{\"device\":\"FAN\",\"action\":\"ON\"}";
-//    // Parse and act
-//    StaticJsonDocument<200> doc;
-//    DeserializationError error = deserializeJson(doc, incomingJson);
-//
-//    if (error) {
-//      Serial.print("JSON Parse failed: ");
-//      Serial.println(error.c_str());
-//      return;
-//    }
-//
-//    String device = doc["device"];
-//    String action = doc["action"];
-//
-//    if (device == "FAN") {
-//      if (action == "ON") {
-//        digitalWrite(fanPin, LOW);
-//        Serial.println("Fan turned ON");
-//      } else if (action == "OFF") {
-//        digitalWrite(fanPin, HIGH);
-//        Serial.println("Fan turned OFF");
-//      }
-//    }
-//  }
 
 //   Only trigger the US sensor every {intervalUS} ms
   // ULTRASONIC SENSOR
@@ -226,16 +325,16 @@ void loop() {
   if (currentmillisUS - previousmillisUS >= intervalUS) {
     previousmillisUS = currentmillisUS;
     distance = usSensor(trigPin, echoPin);
-    sendSensorData("Distance", timestamp, distance);
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.println(" cm");
-    if (distance < 5) {
-      digitalWrite(lampPin, HIGH);
-//      Serial.println("Motion detected within Xcm. Turn LAMP ON");
-    }
-    if (distance > 5) {
-      digitalWrite(lampPin, LOW);
+    Serial.println("Distance: " + String(distance) + " cm");
+//    sendSensorData("Distance", timestamp, distance);
+
+    if (overrideLamp == false) {
+      if (distance < 5) {
+        digitalWrite(lampPin, HIGH);
+      }
+      if (distance > 5) {
+        digitalWrite(lampPin, LOW);
+      }
     }
   }
 
@@ -243,33 +342,42 @@ void loop() {
   unsigned long currentmillisTHERM = millis();
   if (currentmillisTHERM - previousmillisTHERM >= intervalTHERM) {
     previousmillisTHERM = currentmillisTHERM;
-    int rawValue = analogRead(tempPin);
-    double voltage = rawValue * (3.3 / 4095.0);
-    double resistance = (3.3 - voltage) * 10000 / voltage;
-    double temperature = 1 / (log(resistance / 10000) / 3950 + 1 / 298.15) - 273.15;
-    Serial.print("Temperature: ");
-    Serial.print(temperature);
-    Serial.println(" °C");
-    sendSensorData("Temperature", timestamp, temperature);
+
+  double BETA = 3950.0;
+  double raw_value = analogRead(tempPin); 
+  // Read out the voltage using an analog value
+  double voltage = raw_value * 5.0 / 1023.0;
+  // Calculation of the temperature using the voltage
+  double temperature = ((voltage / 5.0) * 10000.0) / (1.0 - (voltage / 5.0));
+  temperature = 1.0 / ((1.0 / 298.15) + (1.0 / BETA) * log(temperature / 10000.0));
+  temperature = temperature - 273.15;
+  // Output of the measured value
+  Serial.println("Temperature: " + String(temperature) + " C");
+//  sendSensorData("Temperature", timestamp, temperature);
+  
+    if (inputString == "tempOVER") {
+      if (overrideFan == false) {
+        digitalWrite(fanPin, LOW);
+      }
     
-    if (temperature > 15 || inputString == "tempOVER") {
-      digitalWrite(fanPin, LOW);
-//      Serial.println("Turn Fan ON");
-      if (pumpIsOn == false) {
-        targetPos += 2048;          // Half rotation forward
-        myStepper.moveTo(targetPos);
+      if (pumpIsOn == false && overridePump == false) {
+//        targetPos += 2048;
+//        myStepper.moveTo(targetPos);
+        myStepper.moveTo(1024);
         pumpIsOn = true;
-//        Serial.println("Turn Water Pump ON");
       }
     }
-    if (temperature < 15 || inputString == "tempUNDER") {
-      digitalWrite(fanPin, HIGH);
-//      Serial.println("Turn Fan OFF");
-      if (pumpIsOn == true) {
-        targetPos -= 2048;          // Half rotation backward
-        myStepper.moveTo(targetPos);
+    
+    if (inputString == "tempUNDER") {
+      if (overrideFan == false) {
+        digitalWrite(fanPin, HIGH);
+      }
+    
+      if (pumpIsOn == true && overridePump == false) {
+//        targetPos -= 2048;
+//        myStepper.moveTo(targetPos);
+        myStepper.moveTo(0);
         pumpIsOn = false;
-//        Serial.println("Turn Water Pump OFF");
       }
     }
   }
@@ -281,18 +389,28 @@ void loop() {
     int airValue = analogRead(gasPin);
     Serial.print("Air quality value: ");
     Serial.println(airValue);  // Print raw sensor value
-    sendSensorData("CO2", timestamp, airValue);
-    if (airValue > 700 || inputString == "airOVER") {
-      windowServo.write(180);
-      digitalWrite(alarmPin, HIGH);
-//      Serial.println("Air quality bad. OPEN WINDOW. ALARM ON");      
+//    sendSensorData("CO2", timestamp, airValue);
+
+    if (inputString == "airOVER") {
+      if (overrideAlarm == false) {
+        digitalWrite(alarmPin, HIGH);
+      }
+    
+      if (overrideWindow == false) {
+        windowServo.write(180);
+      }
     }
-    if (airValue < 700 || inputString == "airUNDER") {
-      windowServo.write(0);
-      digitalWrite(alarmPin, LOW);
-//      Serial.println("Air quality within threshold. CLOSE WINDOW. ALARM OFF");
+    
+    if (inputString == "airUNDER") {
+      if (overrideAlarm == false) {
+        digitalWrite(alarmPin, LOW);
+      }
+    
+      if (overrideWindow == false) {
+        windowServo.write(0);
+      }
     }
   }
-  myStepper.run();
+  
 
 }
